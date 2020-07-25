1de35dc11f6e65dbd01ebe0d8c342b4c9d691d9b
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2020-07-25 15:53:23 -0300

    Add redux provider and routes

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
+import { StatusBar } from "react-native";
+import Routes from "./src/routes";
+import { Provider } from "react-redux";
+import { Store } from "./src/store";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Sardinha APP</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={Store}>
+      <StatusBar barStyle="dark-content" backgroundColor="transparent" />
+      <Routes />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
