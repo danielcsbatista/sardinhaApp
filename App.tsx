72442cb65d77e693f9dc06275d428eef5702a9bc
DANIEL CLEMENTE DOS SANTOS BATISTA <daniel.batista@luizalabs.com>
2020-07-25 18:33:12 -0300

    Add google fonts

--- App.tsx
+++ App.tsx
@@ -3,7 +3,28 @@ import { StatusBar } from "react-native";
 import Routes from "./src/routes";
 import { Provider } from "react-redux";
 import { Store } from "./src/store";
+import { AppLoading } from "expo";
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import {
+  RobotoCondensed_700Bold,
+  RobotoCondensed_400Regular,
+} from "@expo-google-fonts/roboto-condensed";
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    RobotoCondensed_700Bold,
+    RobotoCondensed_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <Provider store={Store}>
       <StatusBar barStyle="dark-content" backgroundColor="transparent" />
